cc4cca2ecf1d7a6ec752e6d30cba072f1af0f454
Vinicius Yu Okubo <vinnco@hotmail.com>
2025-01-25 02:59:08 -0300

    Increasing autoencoder complexity
    + improving loss viz

--- VAE/VAE.py
+++ VAE/VAE.py
@@ -6,17 +6,25 @@ class ConvEncoder(nn.Module):
     def __init__(self, config):
         super(ConvEncoder, self).__init__()
         self.config = config
+
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=4, stride=2, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
 
-        self.conv_mu = nn.Conv2d(in_channels=64, out_channels= self.config['latent_channels'], kernel_size=3, stride=1, padding=1)
-        self.conv_logvar = nn.Conv2d(in_channels=64, out_channels= self.config['latent_channels'], kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        self.conv_mu = nn.Conv2d(in_channels=256, out_channels= self.config['latent_channels'], kernel_size=3, stride=1, padding=1)
+        self.conv_logvar = nn.Conv2d(in_channels=256, out_channels= self.config['latent_channels'], kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x))) # 160 -> 80
-        x = F.relu(self.bn2(self.conv2(x))) # 80 -> 40
+        x = F.relu(self.bn2(self.conv2(x))) # 80 -> 80
+        x = F.relu(self.bn3(self.conv3(x))) # 80 -> 40
+        x = F.relu(self.bn4(self.conv4(x))) # 40 -> 40
 
         mu = self.conv_mu(x)
         logvar = self.conv_logvar(x)
@@ -27,14 +35,25 @@ class ConvDecoder(nn.Module):
         super(ConvDecoder, self).__init__()
         self.config = config
 
-        self.deconv1 = nn.ConvTranspose2d(in_channels=3, out_channels=32, kernel_size=4, stride=2, padding=1)
+        self.deconv1 = nn.ConvTranspose2d(in_channels=config['latent_channels'], out_channels=32, kernel_size=4, stride=2, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=4, stride=2, padding=1)
+        self.deconv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.deconv3 = nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.deconv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        self.deconv5 = nn.ConvTranspose2d(in_channels=256, out_channels=3, kernel_size=4, stride=1, padding=1)
 
     def forward(self, z):
         x = F.relu(z)
         x = F.relu(self.bn1(self.deconv1(x))) # 40 -> 80
-        x = torch.sigmoid(self.deconv2(x)) # 160 -> 80
+        x = F.relu(self.bn2(self.deconv2(x))) # 80 -> 80
+        x = F.relu(self.bn3(self.deconv3(x))) # 80 -> 160
+        x = F.relu(self.bn4(self.deconv4(x))) # 160 -> 160
+        x = torch.sigmoid(self.deconv5(x)) # 160 -> 3
         return x
 
 class VAE(nn.Module):
